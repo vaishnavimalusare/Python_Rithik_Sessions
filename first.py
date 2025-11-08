bf73c06a36923c6fc82f10d7951cd3cdf4b93cee
vaishnavimalusare <vaishvavi1982@gmail.com>
2025-11-08 18:50:38 +0530

    First_Program

---
+++ first.py
@@ -0,0 +1,23 @@
+# print("Addition of Two Numbers")
+# n1=100
+# n2=200   
+# result=n1+n2
+# print("Result is",result)
+# a=340.57777
+# print(type(a))    #type is written by developer for retreving type of data 
+# b="Rithik"   # string should be in double quotes or single quotes 
+# print(type(b))
+# n1=eval(input("Enter first number: ") )  #input function is used to take input from user  string --->int
+# n2=eval(input("Enter second number: "))  
+# print("n1",type(n1))
+# print("n2",type(n2))
+# result=n1+n2
+# print("Result is",result)
+
+
+# si= p*n*r /100 
+# principle- int 
+# n- int 
+# rate float 
+
+print(help("keywords"))
